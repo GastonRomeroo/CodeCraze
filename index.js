3300f66ca21093b28286eaf51c9f99ef76336b71
Gaston Romero <romero_gaston_@hotmail.com>
2023-05-16 20:42:40 -0300

    corrigiendo bucles de javascript

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
+
 let contenedor = document.getElementById("main-container");
 
-//dataJson() WARNING SE CREA UN BUCLE INFINITO
+dataJson() //WARNING SE CREA UN BUCLE INFINITO
 
 function dataJson() {
     fetch('./data/dataDevs.json')
@@ -12,33 +13,6 @@ function dataJson() {
         });
 
 }
-
-
-// function renderDevs(devs) {
-//     contenedor.innerHTML = "";
-//     devs.forEach(item => {
-//         let div = document.createElement('div');
-//         div.className = 'div-container';
-//         let { name, title, hability, img, skills } = item;
-//         div.innerHTML = 
-//         `<div class="card-dev">    
-//             <card class="card">
-//                 <picture class="picture-dev">
-//                     <img class="img-dev" src=${img}>
-//                 </picture>
-//                 <div class="info-dev">
-//                     <h2>${name}</h2>
-//                     <p>${title}</p>
-//                     <p>${hability}</p>
-//                     <p>${skills}<p>
-//                 </div>
-//             </card>
-//         </div>`
-
-//     contenedor.appendChild(div);
-//     dataJson();
-//     });
-// }
 
 function renderDevs(devs) {
 
@@ -56,34 +30,13 @@ function renderDevs(devs) {
         infoDevs.classList.add('info-devs');
         // name devs
         let nameDevs = document.createElement('h2');
-        nameDevs.textContent = devs.name;
+        nameDevs.textContent = devs.alias;
 
 
         cardBody.appendChild(imgCard)
         cardBody.appendChild(infoDevs)
         infoDevs.appendChild(nameDevs)
         contenedor.appendChild(cardBody);
-        dataJson();
     });
 }
 
-
-// // MODAL
-// let openBtn = document.getElementById("open-btn");
-// let modalContainer = document.getElementById("modal-container");
-// let closeBtn = document.getElementById("close-btn");
-
-// // Event listeners
-//  openBtn.addEventListener("click", () => {
-//     modalContainer.style.display = 'block'
-//  })
-
-//  close.addEventListener("click", () => {
-//     modalContainer.style.display = 'none'
-//  })
-
-//  window.addEventListener( "click", (e)=> {
-//     if (e.target === modalContainer){
-//         modalContainer.style.display = "none"
-//     }
-//  })
